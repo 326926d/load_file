b974b0fbc06945741b7536ad74bbf70a50b0e42a
326926d <danzan@list.ru>
2021-12-01 15:54:48 +0300

    change_logic

--- load_test.py
+++ load_test.py
@@ -1,5 +1,5 @@
 # расчет груза при заданных параметрах
-def load_params(l: int, w: int, m: int, h: int):
+def load_params(l: int, w: int, h: int, m: int):
     total_mass = 0
     number_load = 0
     total_height = 0
@@ -10,16 +10,21 @@ def load_params(l: int, w: int, m: int, h: int):
         load_m = float(input('введите массу : '))
         load_params = load_l * load_w
         total_mass += load_m
+        print('общая масса : ', total_mass)
         total_height += load_h
-        if load_params < (l * w) and total_mass < m and total_height < h:
-            print('общая масса : ', total_mass)
-            number_load += 1
-            print('количество мест : ', number_load)
-        else:
-            print('превышение габаритов и массы')
+        if total_mass > m:
+            print('превышена масса груза')
+        elif load_params > (l * w):
+            print('превышены габариты груза')
             break
-    return total_mass - load_m, number_load
+        elif total_height > h:
+            print('превышена погрузочная высота')
+            break
+        else:
+            number_load += 1
+        
+    return total_mass, number_load
 
 
 if __name__ == '__main__':
-    print(load_params(400, 300, 500, 100))
+    print(load_params(400, 300, 150, 500))
